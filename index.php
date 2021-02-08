69bd953a86e4160a733a2f763da02f54d78b8705
Peter <peter.persson66@live.se>
2021-02-08 09:19:28 +0100

    created index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+session_start();
+
+
+
+include 'templates/header-notsignedin.php';
+
+
+
+?>
+
+
+
+<div class="signup-form">
+
+<form action="login.php" method="POST">
+
+<h2 class="login">Login</h2>
+
+<div class="form-group">
+
+<input class="form-control" type="email" name="email" placeholder="E-mail Address">
+
+<span class="input-icon"><i class="fas fa-envelope"></i></span>
+
+</div>
+
+<div class="form-group">
+
+<input type="password" class="form-control" name="password" placeholder="Password">
+
+<span class="input-icon"><i class="fas fa-key"></i></span>
+
+
+
+</div>
+
+<button type="submit" name="submit-login" class="btn btn-primary">Login</button>
+
+<a class="create-acc" href="sign-up.php">Create new account</a>
+
+
+
+</form>
+
+</div>
