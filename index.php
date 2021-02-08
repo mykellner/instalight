557acc5a8cef92f4fcfcd78fc568838eeb3595c8
Peter <peter.persson66@live.se>
2021-02-08 09:28:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,34 +16,32 @@ include 'templates/header-notsignedin.php';
 
 <div class="signup-form">
 
-<form action="login.php" method="POST">
+    <form action="login.php" method="POST">
 
-<h2 class="login">Login</h2>
+        <h2 class="login">Login</h2>
 
-<div class="form-group">
+        <div class="form-group">
 
-<input class="form-control" type="email" name="email" placeholder="E-mail Address">
+            <input class="form-control" type="email" name="email" placeholder="E-mail Address">
 
-<span class="input-icon"><i class="fas fa-envelope"></i></span>
+            <span class="input-icon"><i class="fas fa-envelope"></i></span>
 
-</div>
+        </div>
 
-<div class="form-group">
+        <div class="form-group">
 
-<input type="password" class="form-control" name="password" placeholder="Password">
+            <input type="password" class="form-control" name="password" placeholder="Password">
 
-<span class="input-icon"><i class="fas fa-key"></i></span>
+            <span class="input-icon"><i class="fas fa-key"></i></span>
 
+        </div>
 
+        <button type="submit" name="submit-login" class="btn btn-primary">Login</button>
 
-</div>
-
-<button type="submit" name="submit-login" class="btn btn-primary">Login</button>
-
-<a class="create-acc" href="sign-up.php">Create new account</a>
+        <a class="create-acc" href="sign-up.php">Create new account</a>
 
 
 
-</form>
+    </form>
 
 </div>
